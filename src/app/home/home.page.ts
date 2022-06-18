dbf5b8f79f03ff36b7c198ff997e8f3737674ba5
javierzor <javier20450@gmail.com>
2022-06-18 00:22:22 -0500

    formulario en /formulario muestra respuestas faltan estilos

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { VariosService } from '../service/varios.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -8,23 +9,28 @@ import { VariosService } from '../service/varios.service';
 })
 export class HomePage {
   respuestadetonysvisualizarformularioresuelto: any;
-
   constructor(
     public variosservicios: VariosService,
+    private route: ActivatedRoute
 
 
   ) 
   {
-
-    this.obtenermovimientos();
+    this.traidoporparametrosurl();
+  }
+  
+  async traidoporparametrosurl(){
+    this.route.params.subscribe(params => {
+      this.obtenermovimientos(params);
+      });
   }
 
 
-  obtenermovimientos(){
+  obtenermovimientos(params){
 
     var datatonysvisualizarformularioresuelto = {
       nombre_solicitud: 'tonysvisualizarformularioresuelto',
-      
+      id_publico:params.id_publico
     }
      this.variosservicios.variasfunciones(datatonysvisualizarformularioresuelto).subscribe(async( res: any ) =>{
        console.log('respuesta de tonysvisualizarformularioresuelto', res);
